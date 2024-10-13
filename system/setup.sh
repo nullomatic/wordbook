fb02a8506d7f021a503d3fd47fe11b35a67255b7
nullomatic <nullomatic@pm.me>
2024-10-13 13:39:23 -0500

    added ufw config

--- system/setup.sh
+++ system/setup.sh
@@ -3,9 +3,10 @@
 # Uninstall unofficial Docker packages
 for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
-# Add Docker's official GPG key
 sudo apt-get update
-sudo apt-get install -y ca-certificates curl
+sudo apt-get install -y ca-certificates curl ufw
+
+# Add Docker's official GPG key
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -23,12 +24,22 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plug
 # Set up app directory
 sudo mkdir /app
 sudo useradd -r -s /bin/false -d /app appuser
+sudo usermod -aG docker appuser
 sudo chown -R appuser:appuser /app
 sudo chmod -R 755 /app
+
+# Load group
+sudo newgrp docker
+
+# Firewall
+sudo ufw allow 22
+sudo ufw allow 80
+sudo ufw allow 443
+sudo ufw enable
 
 sudo -u appuser git clone https://github.com/nullomatic/wordbook /app
 
 DIR="$(dirname "$(readlink -f "$0")")"
-cp "$DIR/wordbook.service" /etc/systemd/system/
+sudo cp "$DIR/wordbook.service" /etc/systemd/system/
 sudo systemctl enable --now wordbook.service
 
